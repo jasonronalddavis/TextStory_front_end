c53f0379f3861997a5cdfcd57bbb7d9a663ad43a
jasonronalddavis <jdavis@c.ringling.edu>
2022-01-26 18:42:15 -0600

    checkbox component not connecting to redux store.

--- textstory_frontend/src/components/Form/Storytext/catCheckBox.js
+++ textstory_frontend/src/components/Form/Storytext/catCheckBox.js
@@ -1,70 +1,61 @@
-import React from 'react';
+import React from 'react'
 import {connect} from 'react-redux'
 import { useEffect, useState } from "react";
 import {updateCat} from '../../../action/StoryTexts/CreateStoryTexts';
-import ListCategories from '../../../components/Category/categories';
-import {CreateStoryText} from '../../../action/StoryTexts/CreateStoryTexts';
-import  {fetchCategories}  from '../../../action/Category/fetchCategories.js';
-import CategoriesContainer from '../../../containers/categoriesContainer';
-import StoryTextForm from './storyTextForm'
+import  {fetchCategories}  from '../../../action/Category/fetchCategories';
 
 
-
-
-
-
-
-class  CatCheckBox extends React.Component {
+const CatCheckBox = ({formData, updateCat,fetchCategories,categories}) => {
 
  // const formState = (event,value) => {  const formAttributes = {...formData, [categories]: event.target.value }}
- 
+ console.log(categories)
+
+   const [newImage, setNewImage] = useState('');
+
+  //const initialValues = {name: "", description: "", text_content: "", image_file: "", categories: []}; 
+const [isChecked, setIsChecked] = useState(new Array([].length).fill(false)
+);
 
 
- componentDidMount(){
-    this.props.fetchCategories() 
-    console.log(this.props.categories) 
-    }
 
 
 
-  handleCat = (e, idx) => {
-//   const cat = e.target.checked
-//   const checked = {isChecked: [e.target.value]}
-// //debugger;
-//   setIsChecked(checked ); 
-// updateCat(formData,cat)
+
+
+  const handleCat = (e, idx) => {
+  const cat = e.target.checked
+  const checked = {isChecked: [e.target.value]}
+//debugger;
+  setIsChecked(checked ); 
+updateCat(formData,cat)
   }
 
 
 
 
-render () {
-<CategoriesContainer/>
+
 return (
-    
-    <div className="CheckBx">
- {this.props.categories.map((category,idx) => {
+ categories.map((category,idx) => {
 //debugger;
+
   <label>
   <input
+  className="CheckBox"
   id={category.id}
   type="checkbox"
   value={category.name}
-  onChange={this.handleCat}
-  //checked={isChecked}
+  onChange={handleCat}
+  checked={isChecked}
   name={category.name}
   //onClick={handleChecked}
   />
   </label>
   }
   )
-}
-  </div>
-)  
-}
+)
+   
+
  
-
-
 
 
 
@@ -78,21 +69,17 @@ return (
 
 
 const mapStateToProps = (state) => {
-   // debugger;
+//debugger;
   return {
     formData: state.storytext,
-    categories: state.categories
-  }  
- 
-}
-
-const mapDispatchToProps = dispatch => {
-return {
-categories: () => dispatch(fetchCategories())
-}
+    categories: state.categories,
+    
+  }   
 }
 
 
 
 
-export default connect(mapStateToProps,  {fetchCategories,CategoriesContainer,mapDispatchToProps} )(CatCheckBox)
+
+
+export default connect(mapStateToProps,  {updateCat,fetchCategories} )(CatCheckBox)
